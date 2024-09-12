922646d3902943553a4fb6ea4bf3c78cae86bd3f
LeoUrriste <urristeleonardo@gmail.com>
2024-09-11 23:16:28 -0300

    correccion metodo agregar reseña

--- gestion-eventos/src/main/java/ar/com/eventos/service/reseña/impl/ReseñaServiceImpl.java
+++ gestion-eventos/src/main/java/ar/com/eventos/service/reseña/impl/ReseñaServiceImpl.java
@@ -1,5 +1,7 @@
 package ar.com.eventos.service.reseña.impl;
 
+import ar.com.eventos.domain.Comedor;
+import ar.com.eventos.domain.EventoGastronomico;
 import ar.com.eventos.domain.Reseña;
 import ar.com.eventos.service.reseña.ResenaService;
 
@@ -11,21 +13,28 @@ public class ReseñaServiceImpl implements ResenaService {
 
     @Override
     public Reseña agregarResena(UUID idEvento, Long dniParticipante) {
-
+        Comedor comedor = new Comedor();
+        EventoGastronomico eventoGastronomico = new EventoGastronomico();
         Scanner sc = new Scanner(System.in);
-        String resena = null;
-        Reseña nuevaResena = new Reseña(idEvento,dniParticipante);
+        boolean existeEvento = Boolean.FALSE;
 
+        Reseña nuevaResena = new Reseña(idEvento, dniParticipante);
+
+        for (EventoGastronomico eventoGastronomico1 : comedor.getEventos()) {
+            if (eventoGastronomico.getIdEvento().equals(idEvento)) {
+                existeEvento = Boolean.TRUE;
+            }break;
+
+        }
         System.out.println("Ingrese la reseña:");
         String comentario = sc.nextLine();
         nuevaResena.setResena(comentario);
-
         System.out.println("Ingrese la calificación (un número entero entre 1 y 5):");
         Integer calificacion = sc.nextInt();
         nuevaResena.setCalificacion(calificacion);
 
+        eventoGastronomico.getResenas().put(dniParticipante,nuevaResena);
         System.out.println("Reseña registrada");
-
         return nuevaResena;
     }
 }
